775fd278b0b89d3f6aa34726dd32b3d1c2537f30
studentxz <308795606@qq.com>
2019-10-25 10:44:44 +0800

    传输的视频文件能够分成多个文件，并且每个文件都能播放。

---
+++ UDPServerForRtpTransport_fregmentVideo/Decode/DecodeRTPHelper.cs
@@ -0,0 +1,158 @@
+﻿using System;
+
+namespace UDPServerTest.Decode
+{
+    public class DecodeRTPHelper
+    {
+
+
+        /// <summary>
+        /// 对单一NALU的RTP包进行解包为原始的H264帧，同时输出当前Package的序列号
+        /// </summary>
+        /// <param name="sourceRtpPackage">获取到的单一NALU的RTP包</param>
+        /// <param name="sequenceNumber">序列号</param>
+        /// <returns>原始的H264帧</returns>
+        public static byte[] DecodeSingleNalu(byte[] sourceRtpPackage, int length, ref int index, ref int iDRFrameCount)
+        {
+            ////建立序列号缓存区，并将RTP Package中的序列号拷贝出来
+            //byte[] sequenceNumberBytes = new byte[4];
+            //sequenceNumberBytes[0] = sourceRtpPackage[3];
+            //sequenceNumberBytes[1] = sourceRtpPackage[2];
+            //sequenceNumber = BitConverter.ToInt32(sequenceNumberBytes, 0);
+            //sequenceNumber = 0;
+            //建立原始H264缓存区
+
+            //如果为IDR帧
+            if ((sourceRtpPackage[12] & 0x1f) == 5)
+            {
+                iDRFrameCount++;
+            }
+
+            byte[] sourceH264Bytes = new byte[4 + length - 12];
+            //首先设置H264起始码
+            sourceH264Bytes[0] = 0x00;
+            sourceH264Bytes[1] = 0x00;
+            sourceH264Bytes[2] = 0x00;
+            sourceH264Bytes[3] = 0x01;
+
+            //合并起始码和NALU
+            Array.Copy(sourceRtpPackage, 12, sourceH264Bytes, 4, length - 12);
+            return sourceH264Bytes;
+        }
+
+
+
+        /// <summary>
+        /// 对分片NALU的RTP包进行解包，并将其复制到分片NALU缓存区
+        /// </summary>
+        /// <param name="sourceH264Bbytes">视频流缓存区</param>
+        /// <param name="sourceRtpPackage">通过UDP收到的RTP数据报</param>
+        /// <param name="startCode">起始码字节</param>
+        /// <param name="index">即将插入到视频流缓存区中的索引位置</param>
+        /// <param name="naluHeaderIndex">分片模式中，naluHeader的索引位置</param>
+        /// <param name="fuIndicatorTop3Bit">fu indecator的前三位</param>
+        /// <param name="fuHeaderLast5Bit">fu header的后五位</param>
+        /// <param name="naluHeader">由fu indecator的前三位和fu header的后五位组成的naluHeader</param>
+        /// <returns>分片NALU缓存区</returns>
+        public static byte[] DecodeFregmentNalu(byte[] sourceRtpPackage, byte[] startCode, byte[] fregmentBuffer, int length,
+            ref int fregmentNaluLength, ref int fuIndicatorTop3Bit, ref int fuHeaderLast5Bit,ref int iDRFrameCount,ref int videoSequenceIndex)
+        {
+
+            ////分片NALU缓存区
+            //byte[] fregmentBuffer = new byte[10 * 1024 * 1024];
+
+            //建立序列号缓存区
+            //byte[] sequenceNumberBytes = new byte[2];
+            //sequenceNumberBytes[0] = sourceRtpPackage[3];
+            //sequenceNumberBytes[1] = sourceRtpPackage[2];
+            //sequenceNumber = BitConverter.ToInt32(sequenceNumberBytes, 0);
+
+            //判断该分片为起始，中间还是结尾分片
+            switch (sourceRtpPackage[13] & 0xc0)
+            {
+                //第一分片
+                case 128:
+                    //先确定naluHeader的索引位置
+                    //naluHeaderIndex = index + 4;
+                    //获取FU indicator的前三位F_NRI
+                    fuIndicatorTop3Bit = 0xe0 & sourceRtpPackage[12];
+                    //将startCode存入分片NALU缓存区中
+                    Array.Copy(startCode, 0, fregmentBuffer, 0, 4);
+                    //将第一片NALU存入分片NALU缓存区
+                    Array.Copy(sourceRtpPackage, 14, fregmentBuffer, 5, length - 14);
+                    ////将startCode存入视频流缓存区
+                    //Array.Copy(startCode, 0, sourceH264Bbytes, index, 4);
+                    ////将第一片NALU存入视频流缓存区
+                    //Array.Copy(sourceRtpPackage, 14, sourceH264Bbytes, index+5, length - 14);
+                    ////索引重新赋值
+                    //index += length - 14 + 5;
+                    fregmentNaluLength += length - 14 + 5;
+                    break;
+                //中间分片
+                case 0:
+
+                    Array.Copy(sourceRtpPackage, 14, fregmentBuffer, fregmentNaluLength, length - 14);
+                    //Array.Copy(sourceRtpPackage, 14, sourceH264Bbytes, index, length - 14);
+                    //index += length - 14;
+                    fregmentNaluLength += length - 14;
+                    break;
+                //最后分片
+                default:
+                    //获取FU Header后五位Type
+                    fuHeaderLast5Bit = 0x1f & sourceRtpPackage[13];
+                    var naluHeader = Convert.ToByte(fuHeaderLast5Bit + fuIndicatorTop3Bit);
+                    //给naluHeader赋值
+                    fregmentBuffer[4] = naluHeader;
+
+                    //判断是否为IDR帧
+                    if ((fregmentBuffer[4] & 0x1f) == 5)
+                    {
+                        iDRFrameCount++;
+                    }
+
+
+                    //将最后分片存入分片NALU缓存区中
+                    Array.Copy(sourceRtpPackage, 14, fregmentBuffer, fregmentNaluLength, length - 14);
+                    //将最后一片NALU存入视频流缓存区
+                    //Array.Copy(sourceRtpPackage, 14, sourceH264Bbytes, index, length - 14);
+                    //index += length - 14;
+                    fregmentNaluLength += length - 14;
+
+                    //视频流缓存区中的索引加上分片NALU的总长度
+                    //index += fregmentNaluLength;
+                    ////再将fregmentNaluLength长度置0
+                    //fregmentNaluLength = 0;
+                    break;
+            }
+
+            return fregmentBuffer;
+
+
+
+
+
+
+
+
+            //return fregmentNaluBytes;
+        }
+
+
+
+
+        /// <summary>
+        /// 将spsBuffer  iFrameSequenceBuffer等复制进H264Buffer
+        /// </summary>
+        /// <param name="H264Bytes">H264Buffer</param>
+        /// <param name="sourceBytes">spsBuffer或iFrameSequenceBuffer</param>
+        /// <param name="sourceBytesLength">spsBuffer或iFrameSequenceBuffer中数据的有效长度</param>
+        /// <param name="index">H264Buffer中的索引位置</param>
+        public static void CopyToH264(byte[] H264Bytes, byte[] sourceBytes, int sourceBytesLength, ref int index)
+        {
+            Array.Copy(sourceBytes, 0, H264Bytes, index, sourceBytesLength);
+        }
+
+    }
+}
+
+
---
+++ UDPServerForRtpTransport_fregmentVideo/Program.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using UDPServerTest.Decode;
+
+namespace UDPTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region udpServer
+
+            //建立UDPSocket 参数2：udp协议以数据报的方式传输，参数3：UDP协议
+            Socket udpServer = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            //为udp服务器绑定ip
+            IPAddress ip = IPAddress.Parse(getLocalIpAddress(3));
+            EndPoint ipAddress = new IPEndPoint(ip, 18888);
+            udpServer.Bind(ipAddress);
+
+            //接收数据 本机的所有IP地址，所有可用的端口
+            EndPoint clientAddress = new IPEndPoint(IPAddress.Any, 0);
+
+            #endregion
+
+            #region  缓存区和索引
+            //单个数据报缓存区
+            byte[] data = new byte[1450];
+            //H264缓存区
+            byte[] H264Buffer = new byte[5*1024 * 1024];
+
+            //分片NALU缓存区
+            byte[] fregmentBuffer = new byte[10 * 1024 * 1024];
+
+            //SPS+PPS+SEI缓存区
+            byte[] sps_pps_seiBuffer = new byte[1024 * 1024];
+
+            //建立每一个I帧P帧视频序列缓存区
+            byte[] iFrameAndpFrameSequenceBuffer = new byte[10 * 1024 * 1024];
+
+            //获取到的H.264插入视频缓存区中的位置
+            int index = 0;
+
+            //SPS+PPS+SEI缓存区中的索引位置
+            int spsIndex = 0;
+
+            //每一个I帧P帧视频序列缓存区中索引的位置
+            int videoSequenceIndex = 0;
+
+            #endregion
+
+
+            //收到的RTP Package的length
+            int length = 0;
+
+            //需要分片的NALU的原始的长度
+            int fregmentNaluLength = 0;
+
+            //IDR帧计算
+            int iDRFrameCount = 0;
+
+            //int spsFrameLength = 0;
+            //int ppsFrameLength = 0;
+
+
+            //设置分片NALU原始H264数据的起始码
+            byte[] startCode = new byte[4];
+            startCode[0] = 0x00;
+            startCode[1] = 0x00;
+            startCode[2] = 0x00;
+            startCode[3] = 0x01;
+
+            //设置分片NALU原始H264的NALU Header
+            int fuIndicatorTop3Bit = 0;
+            int fuHeaderLast5Bit = 0;
+            //int naluHeaderIndex = 0;
+            //int rtpCount = 0;
+
+            int fregmentFileCount = 0;
+
+            Console.WriteLine("服务端已开启，监听端口43999");
+
+            //把数据的来源放到第二个参数上
+            while (true)
+            {
+                //收到每一个数据报 
+                length = udpServer.ReceiveFrom(data, ref clientAddress);
+
+                byte[] sequenceNumberBytes = new byte[4];
+                sequenceNumberBytes[0] = data[3];
+                sequenceNumberBytes[1] = data[2];
+                int sequenceNumber = BitConverter.ToInt32(sequenceNumberBytes, 0);
+                //Console.SetCursorPosition(0, 1);
+                Console.WriteLine(sequenceNumber);
+
+                if (length != 1)
+                {
+                    if (sequenceNumber == 3)
+                    {
+                        //首先将SPS+PPS+SEI缓存区中的数据存入每一个H264Buffer最前面
+                        DecodeRTPHelper.CopyToH264(H264Buffer, sps_pps_seiBuffer, spsIndex, ref index);
+                        index += spsIndex;
+                    }
+                    //判断收到的为单一NALU模式
+                    if ((data[12] & 0x1f) != 28)
+                    {
+                        //首先将SPS_PPS_SEI存入SPS+PPS+SEI缓存区中
+                        if (sequenceNumber<=2)
+                        {
+                            var sps_pps_seiBytes = DecodeRTPHelper.DecodeSingleNalu(data, length, ref spsIndex, ref iDRFrameCount);
+                            Array.Copy(sps_pps_seiBytes, 0, sps_pps_seiBuffer, spsIndex, sps_pps_seiBytes.Length);
+                            spsIndex += sps_pps_seiBytes.Length;
+                            continue;
+                        }
+
+                        //获取原始的H.264数据，包含了起始字节00 00 00 01
+                        var singleNalu = DecodeRTPHelper.DecodeSingleNalu(data, length, ref videoSequenceIndex, ref iDRFrameCount);
+
+                        //此时到下一个IDR帧了
+                        if (iDRFrameCount == 2)
+                        {
+                            //加上一个序列之后，长度超出H264Buffer缓存区的长度
+                            if (index + videoSequenceIndex > H264Buffer.Length)
+                            {
+                                //先将之前的输出到文件
+                                Interlocked.Increment(ref fregmentFileCount);
+                                FileStream sourceFileStream = new FileStream(@"D:\vsProject\VS2019Project\FileStorage\Test" +
+                                    Convert.ToInt32(DateTime.Now.TimeOfDay.TotalMilliseconds) + ".264", FileMode.Create);
+                                using (sourceFileStream)
+                                {
+                                    sourceFileStream.Write(H264Buffer, 0, index);
+                                    Console.WriteLine("写入文件Test{0}.264,此时结束原因是1：index + videoSequenceIndex > H264Buffer.Length.", fregmentFileCount);
+                                }
+
+                                //将之前存好的序列存入H264Buffer中
+                                index = spsIndex;
+                                DecodeRTPHelper.CopyToH264(H264Buffer, iFrameAndpFrameSequenceBuffer, videoSequenceIndex, ref index);
+                                index += videoSequenceIndex;
+                                videoSequenceIndex = 0;
+                                
+
+                                //将当前收到的单一NALU存入iFrameAndpFrameSequenceBuffer
+                                Array.Copy(singleNalu, 0, iFrameAndpFrameSequenceBuffer, videoSequenceIndex, singleNalu.Length);
+                                videoSequenceIndex += singleNalu.Length;
+                                iDRFrameCount = 1;
+                            }
+                            else
+                            {
+                                //将IFrameBuffer存入H264Buffer中
+                                DecodeRTPHelper.CopyToH264(H264Buffer, iFrameAndpFrameSequenceBuffer, videoSequenceIndex, ref index);
+                                index += videoSequenceIndex;
+                                videoSequenceIndex = 0;
+                                
+
+                                Array.Copy(singleNalu, 0, iFrameAndpFrameSequenceBuffer, videoSequenceIndex, singleNalu.Length);
+                                videoSequenceIndex += singleNalu.Length;
+                                iDRFrameCount = 1;
+
+                            }
+
+                        }
+                        else
+                        {
+                            //将原始的单个H.264数据存入iFrameAndpFrameSequenceBuffer中
+                            Array.Copy(singleNalu, 0, iFrameAndpFrameSequenceBuffer, videoSequenceIndex, singleNalu.Length);
+                            videoSequenceIndex += singleNalu.Length;
+                        }
+                    }
+                    //收到的为分片NALU模式
+                    else
+                    {
+                        //如果为最后分片
+                        if ((data[13] & 0xc0) == 64)
+                        {
+                            //将所有分片NALU的片段存入fregmentBuffer中
+                            DecodeRTPHelper.DecodeFregmentNalu(data, startCode, fregmentBuffer, length,
+                                ref fregmentNaluLength, ref fuIndicatorTop3Bit, ref fuHeaderLast5Bit,ref iDRFrameCount,ref videoSequenceIndex);
+
+                            //如果检测到收到的分片NALU为IDR帧
+                            if (iDRFrameCount == 2)
+                            {
+
+                                //如果之前已存的视频数据+新存入的iFrameAndpFrameSequenceBuffer的总长度超过H264Buffer的总长度
+                                if (index + videoSequenceIndex > H264Buffer.Length)
+                                {
+                                    //将之前的H264Buffer中的数据先存入文件
+                                    Interlocked.Increment(ref fregmentFileCount);
+                                    FileStream sourceFileStream = new FileStream(@"D:\vsProject\VS2019Project\FileStorage\Test" + 
+                                        Convert.ToInt32(DateTime.Now.TimeOfDay.TotalMilliseconds) + ".264", FileMode.Create);
+                                    using (sourceFileStream)
+                                    {
+                                        sourceFileStream.Write(H264Buffer, 0, index);
+                                        Console.WriteLine("写入文件Test{0}.264,此时结束原因是2：index + videoSequenceIndex > H264Buffer.Length.", fregmentFileCount);
+                                    }
+
+                                    //将之前存好的iFrameAndpFrameSequenceBuffer存入H264Buffer中
+                                    index = spsIndex;
+                                    DecodeRTPHelper.CopyToH264(H264Buffer, iFrameAndpFrameSequenceBuffer, videoSequenceIndex, ref index);
+                                    index += videoSequenceIndex;
+                                    videoSequenceIndex = 0;
+                                    
+
+                                    //将此时收到fregmentBuffer存入iFrameAndpFrameSequenceBuffer中
+                                    Array.Copy(fregmentBuffer, 0, iFrameAndpFrameSequenceBuffer, videoSequenceIndex, fregmentNaluLength);
+                                    videoSequenceIndex += fregmentNaluLength;
+                                    fregmentNaluLength = 0;
+                                    iDRFrameCount = 1;
+
+
+                                }
+                                //如果之前的数据加上刚收的分片NALU的长度没超过H264Buffer的长度
+                                else
+                                {
+                                    DecodeRTPHelper.CopyToH264(H264Buffer, iFrameAndpFrameSequenceBuffer, videoSequenceIndex, ref index);
+                                    index += videoSequenceIndex;
+                                    videoSequenceIndex = 0;
+                                    
+
+                                    //将此时收到fregmentBuffer存入iFrameAndpFrameSequenceBuffer中
+                                    Array.Copy(fregmentBuffer, 0, iFrameAndpFrameSequenceBuffer, videoSequenceIndex, fregmentNaluLength);
+                                    videoSequenceIndex += fregmentNaluLength;
+                                    fregmentNaluLength = 0;
+                                    iDRFrameCount = 1;
+
+
+                                }
+                            }
+                            else
+                            {
+                                //将完整的fregmentBuffer存入iFrameAndpFrameSequenceBuffer中
+                                Array.Copy(fregmentBuffer, 0, iFrameAndpFrameSequenceBuffer, videoSequenceIndex, fregmentNaluLength);
+                                videoSequenceIndex += fregmentNaluLength;
+                                fregmentNaluLength = 0;
+                            }
+
+                        }
+                        //如果不为最后分片
+                        else
+                        {
+                            //将所有分片NALU的片段存入分片NALU缓存区中
+                            DecodeRTPHelper.DecodeFregmentNalu(data, startCode, fregmentBuffer, length,
+                                ref fregmentNaluLength, ref fuIndicatorTop3Bit, ref fuHeaderLast5Bit,ref iDRFrameCount,ref videoSequenceIndex);
+                        }
+
+                    }
+                }
+                else
+                {
+                    Interlocked.Increment(ref fregmentFileCount);
+                    FileStream fs = new FileStream(@"D:\vsProject\VS2019Project\FileStorage\Test" + 
+                        Convert.ToInt32(DateTime.Now.TimeOfDay.TotalMilliseconds) + ".264", FileMode.Create);
+                    using (fs)
+                    {
+                        fs.Write(H264Buffer, 0, index);
+                        Console.WriteLine("写入文件Test{0}.264,此时结束原因是3：rap pacakge length ==1.", fregmentFileCount);
+                        //fs.Close();
+                    }
+                    break;
+                }
+            }
+
+            Console.WriteLine("视频流写入文件完成.");
+            ////将读取到的视频缓存区中的数据存入文件中
+            //FileStream fileStream = new FileStream(@"D:\vsProject\VS2019Project\FileStorage\Test" + Convert.ToInt32(DateTime.Now.TimeOfDay.TotalMilliseconds) + ".264",
+            //    FileMode.Create);
+
+            //using (fileStream)
+            //{
+            //    fileStream.Write(H264Buffer, 0, index);
+            //    Console.WriteLine("获取到的视频流写入文件完成");
+            //    fileStream.Close();
+            //}
+        }
+
+        /// <summary>
+        /// 获取本机IP地址，3：外网IPV4地址，5：局域网IPV4地址
+        /// </summary>
+        /// <param name="addressNumber"></param>
+        /// <returns></returns>
+        private static string getLocalIpAddress(int addressNumber)
+        {
+            //获得本机局域网IP
+            IPAddress[] addressList = Dns.GetHostEntry(Dns.GetHostName()).AddressList;
+
+            if (addressList.Length < 1)
+            {
+                return "";
+            }
+
+            //5是外网IPV4
+            //6是以太网IPV4
+            return addressList[addressNumber].ToString();
+        }
+    }
+}
+
+
